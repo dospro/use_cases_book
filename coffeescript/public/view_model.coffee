344943d9df232658553caad5dae91e5c25a6f4eb
dospro <dospro@gmail.com>
2017-01-31 22:01:53 -0600

    Updated the new case dialog. Now it includes all the data needed to create a use case. Updated the create db script. Some fields were normalized. Updated the view model so it reflects a complete object for a use case.

--- coffeescript/public/view_model.coffee
+++ coffeescript/public/view_model.coffee
@@ -1,9 +1,21 @@
+
+
 class ViewModel
   constructor: ->
     @data = ko.observableArray()
     @form =
       name: ko.observable()
+      version: ko.observable()
       description: ko.observable()
+      goal: ko.observable()
+      actors: ko.observableArray()
+      stakeholders: ko.observableArray()
+      course: ko.observableArray()
+      extensions: ko.observableArray()
+      triggerEvent: ko.observable()
+      preCondition: ko.observable()
+      posCondition: ko.observable()
+      notes: ko.observable()
 
 
     $.getJSON "http://localhost:8081/data"
--- create_db.sql
+++ create_db.sql
@@ -6,13 +6,37 @@ name text not null,
 version text,
 description text,
 goal text,
-actors text,
-stakeholders text,
-course text,
-extensions text,
 trigger_event text,
 pre_condition text,
 pos_condition text,
 notes text,
 creation_date integer
+);
+
+create table if not exists actors(
+id integer primary key not null,
+name text not null
+);
+
+create table if not exists use_case_actors(
+use_case_id integer foreign key not null,
+actor_id integer foreign key not null
+);
+
+create table if not exists stakeholders(
+use_case_id integer foreign key not null,
+stakeholder_name text not null
+)
+
+create table if not exists use_case_steps(
+id integer primary key not null,
+use_case_id integer foreign key not null,
+step_number integer not null,
+description text not null
+);
+
+create table if not exists extensions(
+step_id integer foreign key not null,
+extension_number integer not null,
+description text not null
 );
