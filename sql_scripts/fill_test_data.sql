91e3daf4167d34fae3835380beb7acf31313f822
dospro <dospro@gmail.com>
2017-03-28 21:41:41 -0600

    Issue 1 Add a task to init database. * Added an npm task to run the sql scripts. * Updated knockout. * Added script for filling mock data into the database.

---
+++ sql_scripts/fill_test_data.sql
@@ -0,0 +1,3 @@
+insert into use_cases(name, version, description, goal) values("Make this use case appear.", "1", "Use case description", "Final goal");
+insert into actors(name) values("System");
+insert into actors(name) values("User");
