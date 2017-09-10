4f836ba912017af5084aa503eee756ca05d0b271
dospro <dospro@gmail.com>
2017-09-09 23:56:43 -0500

    Removed sql scripts * Fixed README extension

--- sql_scripts/fill_test_data.sql
+++
@@ -1,3 +0,0 @@
-insert into use_cases(name, version, description, goal) values("Make this use case appear.", "1", "Use case description", "Final goal");
-insert into actors(name) values("System");
-insert into actors(name) values("User");
